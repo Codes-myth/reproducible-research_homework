180e64d58ec97b3219093a513fd8c230067664eb
Felix Smyth <you@example.com>
2024-11-20 15:04:00 +0000

    Editing random walk into reproducible Brownian motion for part c)

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -4,7 +4,17 @@
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
+# Creation of reproducible Brownian motion
+# addition of a random seed into the function allowing for outputs to be reproducible
+# set.seed() function should assign seed input to a specific random walk Brownian
+# motion output
+
+# seed number is now specified after the number of steps argument in the reproducible 
+# random walk function inputs
+
+random_walk  <- function (n_steps, seed) {
+  
+  set.seed(seed)
   
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
   
@@ -28,7 +38,7 @@ random_walk  <- function (n_steps) {
   
 }
 
-data1 <- random_walk(500)
+data1 <- random_walk(500, 12)
 
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
@@ -40,7 +50,7 @@ plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
   ylab("y-coordinate")
 
-data2 <- random_walk(500)
+data2 <- random_walk(500, 12)
 
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   
@@ -53,10 +63,22 @@ plot2 <- ggplot(aes(x = x, y = y), data = data2) +
   ylab("y-coordinate")
 
 
-(randomwalkplots.png <- grid.arrange(plot1, plot2, ncol=2))
+(grid.arrange(plot1, plot2, ncol=2))
+
+# Comparison of the two graphs
+# Both should show the same pattern if the seed argument is working as both have
+# the same seed number input of 12
+
+# The same pattern is shown in both indicating that the seed generation and 
+# reproducibility are working
 
 
-ggsave(randomwalkplots.png, file = "randomwalkplots.png", width = 5.5)
+
+# The steps below were for the previous sections of code to help describe the 
+# patterns seen on the random walk plot
+
+# (randomwalkplots.png <- grid.arrange(plot1, plot2, ncol=2))
+# ggsave(randomwalkplots.png, file = "randomwalkplots.png", width = 5.5)
 
 
 
